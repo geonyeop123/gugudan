357d7274a09da097578e885145b8c3f826dffc7e
geonyeop123 <64470512+geonyeop123@users.noreply.github.com>
2020-08-23 23:37:28 +0900

    8.23
    html / css
    start btn click -> gameForm hide

--- gugudan.js
+++ gugudan.js
@@ -1,1 +1,25 @@
 "use strict";
+const startBtn = document.getElementById("startBtn");
+const gameForm = document.querySelector(".js-gameForm");
+
+let gameStart = false;
+const HIDE_CN = "hide";
+
+function handleStart() {
+  if (gameStart) {
+    startBtn.classList.add(HIDE_CN);
+    gameForm.classList.remove(HIDE_CN);
+  } else {
+    startBtn.classList.remove(HIDE_CN);
+    gameForm.classList.add(HIDE_CN);
+  }
+}
+
+function handleClickSB() {
+  gameStart = true;
+  handleStart();
+}
+
+if (startBtn) {
+  startBtn.addEventListener("click", handleClickSB);
+}
